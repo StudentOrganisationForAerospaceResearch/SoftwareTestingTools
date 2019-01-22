7175e955bac607a0746faffb01448e76bb79848f
Tyler Lam <tylerl264@gmail.com>
2019-01-22 10:29:24 -0700

    added decoder

---
+++ COBS/COBS-C/decoder.c
@@ -0,0 +1,27 @@
+//DECODER FUNCTION
+#include <stdio.h>
+
+void cobs_dec(char *src, char len, char *dst)
+{
+    char *end = src + len;
+    while (src < end)
+    {
+        char i, c = *src++;
+        for (i = 1; i < c; i++)
+            *dst++ = *src++;
+        if (c < 0xff)
+            *dst++ = 0;
+    }
+}
+
+int main(void)
+{
+    //test data in testInput
+    char testInput[12] = {0x03, 0x20, 0x41, 0x04, 0x22, 0x15, 0x17, 0x04, 0x39, 0x21, 0x05, 0x00};
+    char output[sizeof(testInput) - 2];
+    cobs_dec(testInput, sizeof(testInput), output);
+    for (int i = 0; i < sizeof(output); i++)
+    {
+        printf("%02x ", output[i]);
+    }
+}
