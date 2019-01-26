ece34668e3b44d519b231c0fa44e733813d7e591
Jofred Cayabyab <jjay.cayabyab@gmail.com>
2019-01-26 15:02:24 -0700

    add PORT listening

--- COBS/testCobs.py
+++ COBS/testCobs.py
@@ -1,6 +1,6 @@
 import COBS
 import binascii
-
+import serial
 
 def printSignal(in_bytes):
     hex = binascii.hexlify(in_bytes)
@@ -19,8 +19,24 @@ def testSignal(in_bytes):
     print("Decoded: ")
     printSignal(decoded)
 
-input1 = b'\x11\x22\x00\x33'
-input2 = b'\x45\x00\x00\x2c\x4c\x79\x00\x00\x40\x06\x4F\x37'
+# input1 = b'\x11\x22\x00\x33'
+# input2 = b'\x45\x00\x00\x2c\x4c\x79\x00\x00\x40\x06\x4F\x37'
 
-testSignal(input1)
-testSignal(input2)
+# testSignal(input1)
+# testSignal(input2)
+
+
+#"/dev/ttyACM0"
+ser = serial.Serial("COM7", 9600)
+
+# port output
+print(ser.name)
+
+while(True):
+    # print(binascii.hexlify(ser.readline()))
+    line = ser.read(11)
+    print("Original: ")
+    printSignal(line)
+    decoded = COBS.decode(line)
+    print("Decoded: ")
+    printSignal(decoded)
