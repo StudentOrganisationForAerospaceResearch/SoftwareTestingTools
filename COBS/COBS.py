0e59d06b9e57cf10d1d1490331fcef5771b19df1
Jofred Cayabyab <jjay.cayabyab@gmail.com>
2019-01-26 23:21:04 -0700

    General updates and improvements for code clarity and quality - update encode and decode to handle end delimiter character (b'\x00') - add documentation for functions - add error checking for decode - add type checking for encode and decode - add comments for test code - use Camila's implementation instead of cmcqueen

--- COBS/COBS.py
+++ COBS/COBS.py
@@ -1,5 +1,3 @@
-# This implementation is from the cmcqueen cobs-python GitHub implementation.
-
 class DecodeError(Exception):
     pass
 
@@ -18,15 +16,26 @@ def _get_buffer_view(in_bytes):
 def encode(in_bytes):
     """Encode a string using Consistent Overhead Byte Stuffing (COBS).
 
-    Input is any byte string. Output is also a byte string.
+    Input is any byte string or byte array. Output is a byte string.
 
-    Encoding guarantees no zero bytes in the output. The output
-    string will be expanded slightly, by a predictable amount.
+    Encoding guarantees no zero bytes in the output, except for a delimiter
+    byte at the end that signals the end of the packet. The output
+    string will be expanded by 2 for the overhead (starting) byte and the
+    delimiter byte at the end.
 
-    An empty string is encoded to '\\x01'"""
-    if isinstance(in_bytes, str):
-        raise TypeError('Unicode-objects must be encoded as bytes first')
-    in_bytes_mv = _get_buffer_view(in_bytes)
+    An empty string is encoded to '\\x01'
+    """
+
+    # Type checking code (must be bytearray or byte string)
+    if isinstance(in_bytes, bytes):
+        in_bytes_mv = _get_buffer_view(in_bytes)
+    elif not isinstance(in_bytes, bytearray):
+        if isinstance(in_bytes, str):
+            raise TypeError('Unicode-objects must be encoded as bytes first')
+        else:
+            raise TypeError(
+                'Input error: Expected byte string or bytearray object')
+
     final_zero = True
     out_bytes = bytearray()
     idx = 0
@@ -38,6 +47,7 @@ def encode(in_bytes):
             out_bytes += in_bytes_mv[search_start_idx:idx]
             search_start_idx = idx + 1
         else:
+            # Overflow check
             if idx - search_start_idx == 0xFD:
                 final_zero = False
                 out_bytes.append(0xFF)
@@ -47,44 +57,53 @@ def encode(in_bytes):
     if idx != search_start_idx or final_zero:
         out_bytes.append(idx - search_start_idx + 1)
         out_bytes += in_bytes_mv[search_start_idx:idx]
+    # add delimiter byte
+    out_bytes.append(0)
     return bytes(out_bytes)
 
 
 def decode(in_bytes):
     """Decode a string using Consistent Overhead Byte Stuffing (COBS).
 
-    Input should be a byte string that has been COBS encoded. Output
-    is also a byte string.
+    Input should be a byte string or a bytearray object that has been
+    COBS encoded. Output is a byte string.
 
-    A cobs.DecodeError exception will be raised if the encoded data
-    is invalid."""
-    if isinstance(in_bytes, str):
-        raise TypeError(
-            'Unicode-objects are not supported; byte buffer objects only')
-    in_bytes_mv = _get_buffer_view(in_bytes)
+    This output performs error checking to ensure that the incoming
+    byte string has no zeroes."""
+
+    # Type checking code (must be bytearray or byte string)
+    if isinstance(in_bytes, bytes):
+        in_bytes_mv = _get_buffer_view(in_bytes)
+    elif not isinstance(in_bytes, bytearray):
+        if isinstance(in_bytes, str):
+            raise TypeError('Unicode-objects must be encoded as bytes first')
+        else:
+            raise TypeError(
+                'Input error: Expected byte string or bytearray object')
+
     out_bytes = bytearray()
-    idx = 0
+    length = len(in_bytes_mv)
+    index = 0
+    if length <= 0:
+        raise DecodeError("error with input: empty")
+    # Check for zeroes in the input, except for the final delimiter byte.
+    for loop_idx, in_char in enumerate(in_bytes[:-1]):
+        if in_char == 0:
+            raise DecodeError("error with input: zero byte found")
 
-    if len(in_bytes_mv) > 0:
-        while True:
-            length = ord(in_bytes_mv[idx])
-            if length == 0:
-                raise DecodeError("zero byte found in input")
-            idx += 1
-            end = idx + length - 1
-            copy_mv = in_bytes_mv[idx:end]
-            if b'\x00' in copy_mv:
-                raise DecodeError("zero byte found in input")
-            out_bytes += copy_mv
-            idx = end
-            if idx > len(in_bytes_mv):
-                raise DecodeError("not enough input bytes for length code")
-            if idx < len(in_bytes_mv):
-                if length < 0xFF:
-                    out_bytes.append(0)
-            else:
-                break
-    return bytes(out_bytes)
+        # Handle COBS decode logic
+        if(loop_idx == index):
+            # Convert byte string to integer
+            next = int.from_bytes(
+                in_bytes_mv[index], byteorder='big', signed=False)
+            if not index == 0:
+                out_bytes.append(0)
+            out_bytes += in_bytes_mv[index + 1:index + next]
+            index += next
 
-def test():
-    print("Hello world")
+    # If the input is encoded correctly, the index should be on the last element of
+    # the input (the delimiter). Otherwise, throw an error.
+    if(index != length - 1):
+        raise DecodeError(
+            "error with input: not enough input bytes for length code")
+    return out_bytes
--- COBS/testCobs.py
+++ COBS/testCobs.py
@@ -3,11 +3,13 @@ import binascii
 import serial
 
 def printSignal(in_bytes):
+    """Print the signal to the console in a readable manner."""
     hex = binascii.hexlify(in_bytes)
     print(b" ".join(hex[i:i+2] for i in range(0, len(hex), 2)))
 
 
 def testSignal(in_bytes):
+    """Print original signal, encode, print again, decode, print."""
     print("Input: ")
     printSignal(in_bytes)
 
@@ -19,22 +21,31 @@ def testSignal(in_bytes):
     print("Decoded: ")
     printSignal(decoded)
 
-# input1 = b'\x11\x22\x00\x33'
-# input2 = b'\x45\x00\x00\x2c\x4c\x79\x00\x00\x40\x06\x4F\x37'
+# Test code from hard-coded inputs
 
-# testSignal(input1)
-# testSignal(input2)
+input1 = b'\x11\x22\x00\x33'
+input2 = b'\x45\x00\x00\x2c\x4c\x79\x00\x00\x40\x06\x4F\x37'
+input3 = b'\x11\x22\x00\x33\x11\x55\x00\x46\x00'
 
+testSignal(input1)
+testSignal(input2)
+testSignal(input3)
 
-#"/dev/ttyACM0"
+# Test code from encoded inputs
+
+"/dev/ttyACM0"
 ser = serial.Serial("COM7", 9600)
 
 # port output
 print(ser.name)
 
 while(True):
-    # print(binascii.hexlify(ser.readline()))
+    # This should theoretically be a readline() method. However,
+    # the signal that is read is not terminated by a '\n' character.
+    # This should be modified in future code.
     line = ser.read(11)
+
+    # Test
     print("Original: ")
     printSignal(line)
     decoded = COBS.decode(line)
